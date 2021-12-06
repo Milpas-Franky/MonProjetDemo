b85722e16cea6f4dab4af9275f7e552884c90f15
Francois ROLAND <froland@froland.be>
2021-12-06 15:12:40 +0000

    Create user table and model.

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const debug = require("debug")("monprojetdemo:config");
 const favicon = require("serve-favicon");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const studentsRouter = require("./routes/students");
 const classesRouter = require("./routes/classes");
 
@@ -35,7 +34,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/students", studentsRouter);
 app.use("/classes", classesRouter);
 
