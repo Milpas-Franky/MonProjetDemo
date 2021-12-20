b8ab65a9635d5645cca84a7eb0750af1cd6c69f4
Francois ROLAND <froland@froland.be>
2021-12-20 13:47:41 +0000

    Create API to get students from class.

--- routes/classes.js
+++ routes/classes.js
@@ -21,5 +21,32 @@ router.get(
   }
 );
 
+router.get(
+  "/:id/students",
+  passport.authenticate("jwt", { session: false }),
+  async function (req, res, next) {
+    const classId = req.params.id;
+    debug(`List students from class ${classId} [user ${req.user.username}]`);
+    const roles = await req.user.getRoles();
+    if (roles.find((role) => role.name === "admin")) {
+      const students = await Student.findAll({
+        include: [
+          {
+            model: Class,
+            where: {
+              id: classId
+            }
+          }
+        ]
+      });
+
+      res.json(students);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+);
+
+
 module.exports = router;
 
